7051d213425098bd5336f82394c535f2c6221494
kipyek <deniskipyegon88@gmail.com>
2023-08-22 18:21:52 +0300

    NavigateCard greeting improved

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -16,47 +16,47 @@ const NavigateCard = () => {
 
     return (
         <SafeAreaView style={tw`bg-white flex-1`}>
-            <Text style={tw`text-center py-5 text-xl`}>Good Morning, Luke</Text>
+            <Text style={tw`text-center py-5 text-xl`}>Hello, Friend</Text>
             <View style={tw`border-t border-gray-200 flex-shrink`}>
-              <View>
-                  <GooglePlacesAutocomplete 
-                  placeholder='Where To?'
-                  styles={toInputBoxStyles}
-                  fetchDetails={true}
-                  returnKeyType={"search"}
-                  minLength={2}
-                  onPress={(data, details = null) => {
-                     dispatch(
-                         setDestination({
-                         location: details.geometry.location,
-                         description: data.description,
-                     })
-                     );
-                     navigation.navigate('RideOptionsCard')
-                     
-                  }}
-                  enablePoweredByContainer={false}
-                  query={{
-                      key: GOOGLE_MAPS_APIKEY,
-                      language: 'en',
-                  }}
-                  nearbyPlacesAPI='GooglePlacesSearch'
-                  debounce={400}
-                  />
-              </View>
-              <NavFavourites/>
+                <View>
+                    <GooglePlacesAutocomplete
+                        placeholder='Where To?'
+                        styles={toInputBoxStyles}
+                        fetchDetails={true}
+                        returnKeyType={"search"}
+                        minLength={2}
+                        onPress={(data, details = null) => {
+                            dispatch(
+                                setDestination({
+                                    location: details.geometry.location,
+                                    description: data.description,
+                                })
+                            );
+                            navigation.navigate('RideOptionsCard')
+
+                        }}
+                        enablePoweredByContainer={false}
+                        query={{
+                            key: GOOGLE_MAPS_APIKEY,
+                            language: 'en',
+                        }}
+                        nearbyPlacesAPI='GooglePlacesSearch'
+                        debounce={400}
+                    />
+                </View>
+                <NavFavourites />
             </View>
 
             <View style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}>
-                <TouchableOpacity 
-                onPress={() => navigation.navigate("RideOptionsCard")}
-                style={tw`flex flex-row bg-black justify-between w-24 px-4 py-3 rounded-full`}>
-                    <Icon name="car" type="font-awesome" color="white" size={16}/>
+                <TouchableOpacity
+                    onPress={() => navigation.navigate("RideOptionsCard")}
+                    style={tw`flex flex-row bg-black justify-between w-24 px-4 py-3 rounded-full`}>
+                    <Icon name="car" type="font-awesome" color="white" size={16} />
                     <Text style={tw`text-white text-center`}>Rides</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}>
-                    <Icon name="fast-food-outline" type="ionicon" color="black" size={16}/>
+                    <Icon name="fast-food-outline" type="ionicon" color="black" size={16} />
                     <Text style={tw`text-center`}>Eats</Text>
                 </TouchableOpacity>
             </View>
@@ -67,18 +67,18 @@ const NavigateCard = () => {
 export default NavigateCard
 
 const toInputBoxStyles = StyleSheet.create({
-container: {
-    backgroundColor: "white",
-    paddingTop: 20,
-    flex: 0,
-},
-textInput: {
-    backgroundColor: "#DDDDDF",
-    borderRadius: 0,
-    fontSize: 18,
-},
-textInputContainer: {
-    paddingHorizontal: 20,
-    paddingBottom: 0,
-},
+    container: {
+        backgroundColor: "white",
+        paddingTop: 20,
+        flex: 0,
+    },
+    textInput: {
+        backgroundColor: "#DDDDDF",
+        borderRadius: 0,
+        fontSize: 18,
+    },
+    textInputContainer: {
+        paddingHorizontal: 20,
+        paddingBottom: 0,
+    },
 });
