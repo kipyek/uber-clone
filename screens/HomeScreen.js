4d8bdc23f91e5f43504054eb1fb772f23181fa23
kipyek <deniskipyegon88@gmail.com>
2023-08-22 18:17:51 +0300

    minor fix on Home

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -13,52 +13,52 @@ const HomeScreen = () => {
 
     return (
         <SafeAreaView style={tw`bg-white h-full`}>
-         <View style={tw`p-5`}>
-             <Image
-             style={{
-                 width: 100, 
-                 height: 100, 
-                 resizeMode: 'contain'
-             }}
-             source={{
-                 uri: "https://links.papareact.com/gzs",
-             }} />
+            <View style={tw`p-5`}>
+                <Image
+                    style={{
+                        width: 100,
+                        height: 100,
+                        resizeMode: 'contain'
+                    }}
+                    source={{
+                        uri: "https://links.papareact.com/gzs",
+                    }} />
 
-             <GooglePlacesAutocomplete
-             placeholder='Where From?'
-             styles={{
-                 container: {
-                     flex: 0
-                 },
-                 textInput: {
-                     fontSize: 18,
-                 }
-             }}
-             onPress={(data, details = null) => {
-             dispatch(
-                 setOrigin({
-                 location: details.geometry.location,
-                 description: data.description
-             })
-             );
-             
-             dispatch(setDestination(null));
-             }}
-             fetchDetails={true}
-             returnKeyType={"search"}
-             enablePoweredByContainer={false}
-             minLength={2}
-             query={{
-                 key: GOOGLE_MAPS_APIKEY,
-                 language: 'en'
-             }}
-             nearbyPlacesAPI='GooglePlacesSearch'
-             debounce={400}
-             
-             />
-             <NavOptions />
-             <NavFavourites />
-         </View>
+                <GooglePlacesAutocomplete
+                    placeholder='Where From?'
+                    styles={{
+                        container: {
+                            flex: 0
+                        },
+                        textInput: {
+                            fontSize: 18,
+                        }
+                    }}
+                    onPress={(data, details = null) => {
+                        dispatch(
+                            setOrigin({
+                                location: details.geometry.location,
+                                description: data.description
+                            })
+                        );
+
+                        dispatch(setDestination(null));
+                    }}
+                    fetchDetails={true}
+                    returnKeyType={"search"}
+                    enablePoweredByContainer={false}
+                    minLength={2}
+                    query={{
+                        key: GOOGLE_MAPS_APIKEY,
+                        language: 'en'
+                    }}
+                    nearbyPlacesAPI='GooglePlacesSearch'
+                    debounce={400}
+
+                />
+                <NavOptions />
+                <NavFavourites />
+            </View>
         </SafeAreaView>
     )
 }
